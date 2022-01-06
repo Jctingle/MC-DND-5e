58300b0be24c0cf99ea7f8c6f53f35bf610d594b
Jefferson Tingle <jeffersonctingle@gmail.com>
2022-01-06 15:54:08 -0500

    Mobicon development cont.
    Protected data and adjusted information storage after learning particulars of scoreboard tags

--- mobicon.java
+++ mobicon.java
@@ -40,14 +40,12 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
     public mobicon(App app){
         this.app = app;
     }
+    //these can be overridden by permissions, well some of them
     ArrayList<Player> tokenPlacer = new ArrayList<>();
     ArrayList<Player> dungeonMaster = new ArrayList<>();
-    String mobType = new String();
-    Double health = (double) 10;
-    String mobName = new String();
-    double AC = (double) 0.0;
+    Map<Player, ArrayList<String>> informationHold = new HashMap<>();
     Map<Player, ArrayList<String>> unitSpawner = new HashMap<>();
-    boolean hasType = false;
+    //will need an onmobdeath reaction for when they are murdered by hand or /killed to remove them from the list of units in unitSpawners
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
         if(sender instanceof Player){
@@ -56,10 +54,22 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
                 tokenPlacer.add(p);
                 //need if's or situation proofers
                     p.sendMessage("Creating Token" + args[1]);
-                    mobType = args[1].toString();
-                    mobName = args[2].toString();
-                    health = Double.parseDouble(args[3]);
-                    AC = Double.parseDouble(args[4]);
+                    String mobType = args[1].toString();
+                    String mobName = args[2].toString();
+                    String health = args[3].toString();
+                    String AC = args[4].toString();
+                    ArrayList<String> temphold = new ArrayList<>();
+                    temphold.add(mobType);
+                    temphold.add(mobName);
+                    temphold.add(health);
+                    temphold.add(AC);
+                    if (args[5] != null && args[5].equals("pc")){
+                        temphold.add("PC");
+                        informationHold.put(p, temphold);
+                    }
+                    else{
+                        informationHold.put(p, temphold);
+                    }
                 // noteText = args[0];
                                         }
             return true;
@@ -77,7 +87,15 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
                 RayTraceResult rtx = p.getWorld().rayTraceBlocks(p.getEyeLocation(), p.getEyeLocation().getDirection(), 100);
                 if (rtx != null){
                     Location pSpot = rtx.getHitPosition().toLocation(p.getWorld());
-                    //change this to summon the armor stand
+
+                    //deconstructor I guess
+                    ArrayList<String> tokenInfo = informationHold.get(p);
+                    String mobType = tokenInfo.get(0).toString();
+                    String mobName = tokenInfo.get(1).toString();
+                    Double health = Double.parseDouble(tokenInfo.get(2).toString());
+                    Double AC = Double.parseDouble(tokenInfo.get(3).toString());
+                    //end deconstructor
+
                     Location pSpotCopy = pSpot.clone();
                     pSpotCopy.setY(pSpot.getY() - 2);
                     LivingEntity token = (LivingEntity) p.getLocation().getWorld().spawnEntity(pSpot,EntityType.valueOf(mobType));
@@ -89,14 +107,17 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
                     token.setCustomName(mobName);
                     token.setCustomNameVisible(true);   
                     token.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS).setBaseValue(AC);
-
-                    //find some way to set health and track it, as well as AC, and interact with health
+                    token.setInvulnerable(true);
+                    token.addScoreboardTag("token");
+                    informationHold.remove(p);
+                    //add PC tag for player tokens
                     }
                 else{
                     p.sendMessage("Please select solid ground");
                 }
             }
             //right click on the specific mob, and gets their info
+            //need a check for OP and/or DM player
             else if(unitSpawner.containsKey(e.getPlayer())){
                 Player p1 = (Player) e.getPlayer();
                 ArrayList tempOwned = unitSpawner.get(p1);
@@ -107,6 +128,7 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
                     String tempAC = tempEnt.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS).toString();
                     String tempHealth =  "" + tempEnt.getHealth();
                     String tempTempHP = "" + tempEnt.getAbsorptionAmount();
+                    //need a switch with cases here; have to provide sitations for when tempHP is completely empty
                     p1.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("" + tempName + " AC: " + tempAC + " CurrentHealth: " + tempHealth + " tempHP: " + tempTempHP));
                 }
             }
@@ -120,7 +142,7 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
             switch (modType){
                 case 0:
                     //healing
-                    //check if current health is .5 AKA in death saving state
+                    //check for scoreboardtag Deathsaving, then remove because healing
                     //check for maxhealth value
                     tempHealth = tempHealth += modim;
                 case 1:
@@ -130,11 +152,19 @@ public class mobicon implements CommandExecutor,Listener,TabCompleter {
                     tempHealth = tempHealth -= modim;
                 case 2:
                     //tempHP
+                    //also check for deathsaving
                     //get absorption level, then check if absorb/thp greater than damage double, if greater than do nothing else set value to new temp HP
             }
     }
+    public void setDeathSave(LivingEntity playerToken){
+        //going to addscoreboardtag, have to determine a way of marking it as a player tag
+        playerToken.addScoreboardTag("deathSaving");
+        //command for fake killing a player token AKA putting in death saving mode
+    }
     public void deleteMob(LivingEntity deceased, Player owner){
             //delete mob from existence
+            //run this function/call it from other situations.
+            //actual mob token deletion will depend on current state of mob
             //remove value from player key if player has more than one unit, or remove key and value if just the one unit
             if (unitSpawner.get(owner).size() >= 2){
                 ArrayList<String> removedList = unitSpawner.get(owner);
